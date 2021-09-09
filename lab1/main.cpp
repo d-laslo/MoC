75b95499c1b116f49a0d1eb0c2f4b2b2b67746ed
d-laslo <ice_bear@ukr.net>
2021-09-09 14:44:53 +0300

    add table conversion

--- lab1/main.cpp
+++ lab1/main.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 #include <string>
 #include <fstream>
+#include <vector>
 #include "headers/lab1.hpp"
 #include "headers/source.hpp"
 
@@ -21,14 +22,14 @@ int main(int argc, char const *argv[])
     std::string var_num = argv[1];
     var_num = (var_num.length() > 1 ? "" : "0") + var_num;
 
+    std::vector<std::vector<double>> prob;
+    std::vector<std::vector<int>> table;
     try {
         fprob.open("vars/prob_" + var_num + ".csv");
-        std::vector<std::vector<double>> prob;
         if (!parse_data(prob, fprob)) throw -2;
         fprob.close();
 
         ftable.open("vars/table_" + var_num + ".csv");
-        std::vector<std::vector<double>> table;
         if (!parse_data(table, ftable)) throw -2;
         ftable.close();
     }
@@ -36,6 +37,22 @@ int main(int argc, char const *argv[])
         std::cerr << "Parsing error";
         return a;
     }
+    
+    auto set_indexes = [](const std::vector<std::vector<int>>& table, std::vector<std::vector<std::vector<int>>>& result) 
+    {
+        for (int k_indx = 0; k_indx < table.size(); k_indx++) {
+            for (int plt_indx = 0; plt_indx < table[k_indx].size(); plt_indx++) {
+                result[ table[k_indx][plt_indx] ].push_back( {k_indx, plt_indx} ); 
+            }
+        }
+    };
+
+    // stores the plaintext and key indices for the ciphertext 
+    std::vector<std::vector<std::vector<int>>> indexes(C);
+    set_indexes(table, indexes);
+
+    
+    
     
     return 0;
 }
