0888b65ddcbb008f7b5740b3162b3581e7f00f1d
d-laslo <ice_bear@ukr.net>
2021-09-09 18:10:23 +0300

    add ciphertext distribution

--- lab1/headers/lab1.hpp
+++ lab1/headers/lab1.hpp
@@ -1,1 +1,11 @@
 #pragma once
+#include <vector>
+#include <numeric>
+#include <map>
+
+
+const int M = 20;
+const int C = 20;
+const int K = 20;
+
+void ciphertext_probability_distribution(const std::vector<std::vector<std::map<std::string, int>>>&table, const std::map<std::string, std::vector<double>>& prob, std::vector<double>& result);
--- lab1/source/lab1.cpp
+++ lab1/source/lab1.cpp
@@ -1,1 +1,13 @@
 #include "../headers/lab1.hpp"
+
+void ciphertext_probability_distribution(const std::vector<std::vector<std::map<std::string, int>>>& table, const std::map<std::string, std::vector<double>>& prob, std::vector<double>& result)
+{
+    for (u_int64_t i = 0; i < table.size(); i++) {
+        result.push_back(std::accumulate(table[i].begin(), table[i].end(), 0.,
+            [&prob](double result, const std::map<std::string, int>& index)
+            {
+                return result + prob.at("key")[index.at("key")] * prob.at("plaintext")[index.at("plaintext")];
+            }
+        ));
+    }
+}
