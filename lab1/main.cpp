fbfb9a83d1ac3c80e27377796af0ebacec65eb9b
d-laslo <ice_bear@ukr.net>
2021-09-09 18:16:25 +0300

    namespace added

--- lab1/main.cpp
+++ lab1/main.cpp
@@ -7,7 +7,7 @@
 #include "headers/lab1.hpp"
 #include "headers/source.hpp"
 
-
+using namespace std;
 
 int main(int argc, char const *argv[])
 {
@@ -18,16 +18,16 @@ int main(int argc, char const *argv[])
         }
     }
     catch (int a) {
-        std::cerr << "Not enough arguments\n";
+        cerr << "Not enough arguments\n";
         return a;
     }
     
-    std::ifstream fprob, ftable;
-    std::string var_num = argv[1];
+    ifstream fprob, ftable;
+    string var_num = argv[1];
     var_num = (var_num.length() > 1 ? "" : "0") + var_num;
 
-    std::vector<std::vector<double>> prob;
-    std::vector<std::vector<int>> table;
+    vector<vector<double>> prob;
+    vector<vector<int>> table;
     try {
         fprob.open("vars/prob_" + var_num + ".csv");
         if (parse_data(prob, fprob)) throw -2;
@@ -38,12 +38,12 @@ int main(int argc, char const *argv[])
         ftable.close();
     }
     catch(int a) {
-        std::cerr << "Parsing error";
+        cerr << "Parsing error";
         return a;
     }
 
     // stores the plaintext and key indices for the ciphertext 
-    std::vector<std::vector<std::map<std::string, int>>> ciphertext_indexes(C);
+    vector<vector<map<string, int>>> ciphertext_indexes(C);
     for (u_int64_t k_indx = 0; k_indx < table.size(); k_indx++) {
         for (u_int64_t plt_indx = 0; plt_indx < table[k_indx].size(); plt_indx++) {
             ciphertext_indexes[ table[k_indx][plt_indx] ].push_back({
@@ -54,12 +54,12 @@ int main(int argc, char const *argv[])
     }
 
     // stores the probabilities of the plaintexts and keys
-    std::map<std::string, std::vector<double>> probabilities {{"plaintext", prob[0]}, {"key", prob[1]}};
+    map<string, vector<double>> probabilities {{"plaintext", prob[0]}, {"key", prob[1]}};
 
 
-    std::vector<double> ciphertext_dist;
+    vector<double> ciphertext_dist;
     ciphertext_probability_distribution(ciphertext_indexes, probabilities, ciphertext_dist);
-    
-        
+
+
     return 0;
 }
