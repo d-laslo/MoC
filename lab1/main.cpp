35b2ee13309f8d9bd8b43f1307779bc039b7fbb7
d-laslo <ice_bear@ukr.net>
2021-09-09 18:14:04 +0300

    reshape

--- lab1/main.cpp
+++ lab1/main.cpp
@@ -2,8 +2,12 @@
 #include <string>
 #include <fstream>
 #include <vector>
+#include <map>
+#include <numeric>
 #include "headers/lab1.hpp"
 #include "headers/source.hpp"
+
+
 
 int main(int argc, char const *argv[])
 {
@@ -26,33 +30,36 @@ int main(int argc, char const *argv[])
     std::vector<std::vector<int>> table;
     try {
         fprob.open("vars/prob_" + var_num + ".csv");
-        if (!parse_data(prob, fprob)) throw -2;
+        if (parse_data(prob, fprob)) throw -2;
         fprob.close();
 
         ftable.open("vars/table_" + var_num + ".csv");
-        if (!parse_data(table, ftable)) throw -2;
+        if (parse_data(table, ftable)) throw -2;
         ftable.close();
     }
     catch(int a) {
         std::cerr << "Parsing error";
         return a;
     }
-    
-    auto set_indexes = [](const std::vector<std::vector<int>>& table, std::vector<std::vector<std::vector<int>>>& result) 
-    {
-        for (int k_indx = 0; k_indx < table.size(); k_indx++) {
-            for (int plt_indx = 0; plt_indx < table[k_indx].size(); plt_indx++) {
-                result[ table[k_indx][plt_indx] ].push_back( {k_indx, plt_indx} ); 
-            }
-        }
-    };
 
     // stores the plaintext and key indices for the ciphertext 
-    std::vector<std::vector<std::vector<int>>> indexes(C);
-    set_indexes(table, indexes);
+    std::vector<std::vector<std::map<std::string, int>>> ciphertext_indexes(C);
+    for (u_int64_t k_indx = 0; k_indx < table.size(); k_indx++) {
+        for (u_int64_t plt_indx = 0; plt_indx < table[k_indx].size(); plt_indx++) {
+            ciphertext_indexes[ table[k_indx][plt_indx] ].push_back({
+                {"key", k_indx}, 
+                {"plaintext", plt_indx}
+            }); 
+        }
+    }
 
+    // stores the probabilities of the plaintexts and keys
+    std::map<std::string, std::vector<double>> probabilities {{"plaintext", prob[0]}, {"key", prob[1]}};
+
+
+    std::vector<double> ciphertext_dist;
+    ciphertext_probability_distribution(ciphertext_indexes, probabilities, ciphertext_dist);
     
-    
-    
+        
     return 0;
 }
