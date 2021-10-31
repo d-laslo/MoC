620df7536680fde413cecf7fe9bd1010685d6455
vchikhladze <vchikhladze@vchikhladzes-MacBook-Pro.local>
2021-10-31 15:18:33 +0200

    removed unused code

--- lab2/frequency.py
+++ lab2/frequency.py
@@ -4,14 +4,6 @@ from source import write, read
 
 path = os.path.dirname(os.path.abspath(__file__))
 
-# def  letter_frequency(text):
-#     letters = list(set(text))
-#     return {i:text.count(i) for i in letters}
-
-# def bigram_frequency(text):
-#     bigrams_list = [ text[i:i+2] for i in range(len(text) - 1)]
-#     bigrams = list(set(bigrams_list))
-#     return {i:bigrams_list.count(i) for i in bigrams}
 
 def letter_frequency(text):
     return n_gram_frequency(text, 1)
