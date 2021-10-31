43fa42752f4c7b147657ba447c16a180fed50c41
d-laslo <ice_bear@ukr.net>
2021-11-01 00:43:17 +0200

    update tresholds

--- lab2/criteria.py
+++ lab2/criteria.py
@@ -42,37 +42,37 @@ class Criterion:
             return True
         return False
 
-    def criterian23(self, text: str, set_size: int, thresholds: list[int]) -> bool:
+    def criterian23(self, text: str, set_size: int, thresholds: list[int]) -> bool: # 10: <30, 1>, 100: <7, 4>, 1000: <20, 150>, 10000: <20, 1700>
         text_n_grams = self.__split_into_n_grams(text, self.__n_grams_len)
         allowed_n_grams = set(self.__n_grams[:set_size])
 
         frequencies = sorted([text_n_grams.count(i) for i in allowed_n_grams])
-        if sum(frequencies) >= sum(thresholds):
+        if sum(frequencies) >= thresholds:
             return True
         return False
 
-    def criterian40(self, text: str, threshold: int) -> bool:
+    def criterian40(self, text: str, threshold: int) -> bool: #10: .03, 100: .003, 1000: .001, 10000: .00035
         I = self.__compliance_index(n_gram_frequency(text, self.__n_grams_len))
 
         if abs(I - self.__compliance_index_language) > threshold:
             return False
         return True
 
-    def criterian50(self, text: str, set_size: int, threshold: int) -> bool:
-        allowed_n_grams = self.__n_grams[-set_size:]   
+    def criterian50(self, text: str, set_size: int, threshold: int) -> bool: # 10: <500, 497>, 100: <100, 99>, 1000: <50, 49>, 10000: <50, 47>
+        allowed_n_grams = set(self.__n_grams[-set_size:])
         frequency = n_gram_frequency(text, self.__n_grams_len)
 
-        count = sum([
-            1 if frequency[x] > 0 else 0 
-            for x in allowed_n_grams
-        ])
+        count = len(set(frequency.keys()) & allowed_n_grams)
         if (set_size - count) <= threshold:
             return False
         return True
     
-    def structuralCriterian(self, path_to_text: str, threshold: float) -> bool:
-        archive = zipfile.ZipFile(f'{path}/tmp.zip', 'w')
+    def structuralCriterian(self, path_to_text: str, threshold: float) -> bool: # 10: --, 100: --, 1000: 48, 10000: 66.2
+        archive = zipfile.ZipFile(f'{path}/tmp.zip', 'w', zipfile.ZIP_DEFLATED)
         archive.write(path_to_text)
+        archive.close()
+        tt = os.stat(path_to_text).st_size
+        ttt = os.stat(f'{path}/tmp.zip').st_size
         statinfo = 100 * abs(os.stat(path_to_text).st_size - os.stat(f'{path}/tmp.zip').st_size) / os.stat(path_to_text).st_size
         if statinfo > threshold:
             return True
@@ -85,7 +85,7 @@ def main_test():
     criterion = Criterion(bigrams_frequency)
     res = []
     for i in range(1, 1001):
-        res.append(criterion.criterian22(read(f'{path}/texts/X/L_10000_N_{i}.log'), 30, 40))
+        res.append(criterion.structuralCriterian(f'{path}/texts/X/L_10000_N_{i}', 66.2))
     print(res.count(True))
 
 if __name__ == '__main__':
